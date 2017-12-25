d854dd5898a5671cf89f9ce129a4b77e30a37a12
jackpotseed <riris8756@naver.com>
2017-12-25 23:43:29 +0900

    TODO postman install

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ var app 	= express();
 //post 데이터처리
 var bodyParser = require('body-parser');
 //세션관리
-var session = require('express-session');
-var fs = require("fs")
-
-var router  = require('./router/main')(app);
-
-app.use(express.static(__dirname + '/public'));
-console.log("__dirname" + __dirname);
+/*var session = require('express-session');*/
+//파일을 열기위함
+var fs = require("fs");
 
 app.set('views','./views');
 app.set('view engine', 'ejs');
@@ -18,5 +14,9 @@ app.engine('html', require('ejs').renderFile);
 var server  = app.listen(3000, function() {
 
 	console.log("express server has started on port 3000 ");
-
 })
+
+app.use(express.static(__dirname + '/public'));
+console.log("__dirname" + __dirname);
+
+var router  = require('./router/main')(app, fs);
